a24f2d580da12f28765dabd28158b8b8f2762c8e
vedcoder <gupta.vidit@icloud.com>
2018-12-23 13:25:05 +0530

    Update arguments.py

--- class folder/arguments.py
+++ class folder/arguments.py
@@ -1,17 +1,17 @@
-def hello_f(greetings = "hi",name=" you"): # 1st case
+def hello_f(greetings = "hi",name=" you"): # 1st case to make it work
     return "{}{}" . format(greetings,name)
 print(hello_f())  
 
 
 
 
-def hello_a(greetings,name=" you"):#2nd case
+def hello_a(greetings,name=" you"): # 2nd case to make it work
     return "{}{}" . format(greetings,name)
 print(hello_f(greetings = "hi",name=" vidit")) 
 
 
 
 
-def hello_b(greetings,name=" you"):# case 3
+def hello_b(greetings,name=" you"): # 3rd case to make it work 
     return "{}{}" . format(greetings,name)
 print(hello_f("hi ","vidit")) 
