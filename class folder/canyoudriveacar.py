37ede8b10ad13401d929357aeb6fda5d5350be6e
shivani jain <gupta.vidit@icloud.com>
2018-12-23 10:52:30 +0530

    more things

---
+++ class folder/canyoudriveacar.py
@@ -0,0 +1,8 @@
+def canyoudrive():
+    x = input("what is your name: ")
+    y = int(input("what is your age: "))
+    if y >= 18:
+        return "you can drive a car"
+    else:
+        return "you cannot drive a car"    
+print(canyoudrive())        
