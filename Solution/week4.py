a7d0e1d2e918f1e9e8fe518c8f92a4fa6a1d4827
shivani jain <gupta.vidit@icloud.com>
2018-12-27 22:26:58 +0530

    submission

---
+++ Solution/week4.py
@@ -0,0 +1,31 @@
+# 4.1
+def maxnumber():
+    x = int(input("please enter a number: "))
+    y = int(input("please enter a number: "))
+    z = int(input("please enter a number: "))
+    return "the max the numbers you enter is ",max(x,y,z)
+print(maxnumber())    
+
+#4.2
+def sum1():
+    a = int(input("please enter a number: "))
+    b = int(input("please enter a number: "))
+    c = int(input("please enter a number: "))
+    d = a+b+c
+    return "the sum of the numbers that you entered is ",d
+print(sum1)) 
+
+#4.3    
+def multiply():
+    e = int(input("please enter a number: "))
+    f = int(input("please enter a number: "))
+    g = int(input("please enter a number: "))
+    h = e*f*g
+    return "the sum of the numbers that you entered is ",h
+print(multiply()) 
+
+#4.4
+i = input("pleae enter a string: ")
+print("the reverse of your string is: ",i[::-1])
+
+#4.5
