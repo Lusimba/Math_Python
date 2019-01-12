accbca9a8366b749894b09683568c04bf993d5d1
Vidit Gupta <gupta.vidit@icloud.com>
2019-01-12 10:01:52 +0530

    hello

--- Solution/week4.py
+++ Solution/week4.py
@@ -170,7 +170,7 @@ if __name__ == "__main__":
     gui = Tk() 
     gui.configure(background="light green") 
     gui.title("Simple Calculator")  
-    gui.geometry("265x125") 
+    gui.geometry("00x300") 
     equation = StringVar()  
     expression_field = Entry(gui, textvariable=equation) 
     expression_field.grid(columnspan=4, ipadx=70) 
